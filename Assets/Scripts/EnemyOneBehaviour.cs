fb332dbb4d1af249b333d467a3a875e48137ce0d
Andrew Mokhtary <akouroshm@gmail.com>
2023-11-10 22:20:45 -0500

    Coins additions added

---
+++ Assets/Scripts/EnemyOneBehaviour.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyOneBehaviour : MonoBehaviour
+{
+    public GameObject explosionPrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(new Vector3(0, -1, 0) * Time.deltaTime * 3);
+        if (transform.position.y < -8f)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D whatIHit)
+    {    
+        if(whatIHit.tag == "Player")
+        {
+            Instantiate(explosionPrefab, transform.position, Quaternion.identity);
+            Destroy(this.gameObject);
+        }
+        else if (whatIHit.tag == "Weapon")
+        {
+            GameObject.Find("GameManager").GetComponent<GameManager>().EarnScore(1);
+            Instantiate(explosionPrefab, transform.position, Quaternion.identity);
+            Destroy(whatIHit.gameObject);
+            Destroy(this.gameObject);
+        }
+    }
+}
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,16 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
+using TMPro;
 public class GameManager : MonoBehaviour
 {
     public GameObject EnemyOnePrefab;
     public GameObject EnemyTwoPrefab;
+    public GameObject CoinPrefab;
+    public int score;
+    public TextMeshProUGUI scoreText;
     // Start is called before the first frame update
     void Start()
     {
         InvokeRepeating("CreateEnemyOne", 1.0f, 3.0f);
         InvokeRepeating("CreateEnemyTwo", 2.0f, 5.0f);
+        InvokeRepeating("CreateCoin", 1.0f, 15.0f);
+        score = 0;
+        scoreText.text = "Score: " + score;
     }
 
     // Update is called once per frame
@@ -27,5 +34,15 @@ public class GameManager : MonoBehaviour
     void CreateEnemyTwo()
     {
         Instantiate(EnemyTwoPrefab, new Vector3(Random.Range(-8, 8), 7, 0), Quaternion.identity);
+    }
+    void CreateCoin()
+    {
+        Instantiate(CoinPrefab, new Vector3(Random.Range(-8, 8), 7, 0), Quaternion.identity);
+    }
+
+    public void EarnScore(int scoreToAdd)
+    {
+        score = score + scoreToAdd;
+        scoreText.text = "Score: " + score;
     }
 }
